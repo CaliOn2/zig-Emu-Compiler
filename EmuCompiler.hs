8e4ef0404d094b32ef77d78f38b1508fd69784ef
K's Thinkpad <K.T.Kraft@protonmail.com>
2024-10-02 10:43:16 +0200

    added gitignore and file reading

--- EmuCompiler.hs
+++ EmuCompiler.hs
@@ -1,2 +1,14 @@
-f :: a -> b
-f x = undefined 
+import System.IO
+import System.Environment
+
+outputFile sourcefilename = do
+  sfHandle <- openFile sourcefilename ReadMode
+  putStrLn "Hello, World!"
+  sfContents <- hGetContents sfHandle
+  print sfContents
+
+main = do
+  args <- getArgs
+  if (length args) > 0
+    then outputFile (args !! 0)
+    else putStrLn "This is a Compiler For K's Emulator"
